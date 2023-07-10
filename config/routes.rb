43a175c309b4548c8ef9c5e24c12144add480879
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-10 22:42:57 +0900

    Define routing for lyrics controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'lyrics/index'
-  get 'lyrics/new'
-  get 'lyrics/edit'
-  get 'lyrics/destroy'
-  get 'lyrics/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'lyrics#index'
+  resources :lyrics, only: [:new, :create, :edit, :update, :destroy, :show]
 end
