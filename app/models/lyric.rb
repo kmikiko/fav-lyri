3c0f09e9924d7330d72c446c2347c26945baf3ce
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-11 11:54:05 +0900

    Add link new to index #3

--- app/models/lyric.rb
+++ app/models/lyric.rb
@@ -1,8 +1,8 @@
 class Lyric < ApplicationRecord
   validates :phrase, presence: true, length: { maximum: 20 }
   validates :detail, presence: true
-  has_one :artist
-  has_one :song
+  has_one :artist, dependent: :destroy
+  has_one :song, dependent: :destroy
   accepts_nested_attributes_for :artist, allow_destroy: true
   accepts_nested_attributes_for :song, allow_destroy: true
 end
