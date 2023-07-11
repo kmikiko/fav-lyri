f0b8ae2be5f142f74dc61fee01879e5c384c0159
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-11 10:54:13 +0900

    Add Song model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_11_015205) do
+ActiveRecord::Schema.define(version: 2023_07_11_015353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,5 +30,14 @@ ActiveRecord::Schema.define(version: 2023_07_11_015205) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
+    t.bigint "lyric_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lyric_id"], name: "index_songs_on_lyric_id"
+  end
+
   add_foreign_key "artists", "lyrics"
+  add_foreign_key "songs", "lyrics"
 end
