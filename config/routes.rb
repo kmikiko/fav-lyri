8b0cd7de54593a64f9245a9f4cdb8bc064affb90
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-11 23:41:02 +0900

    Create routing for letter_opener

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'lyrics#index'
   resources :lyrics, only: [:new, :create, :edit, :update, :destroy, :show]
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
