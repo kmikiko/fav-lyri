e4db5383ece98895bda90912290a4fc0bac19de8
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-11 10:53:26 +0900

    Add Artist model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_10_132254) do
+ActiveRecord::Schema.define(version: 2023_07_11_015205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.bigint "lyric_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lyric_id"], name: "index_artists_on_lyric_id"
+  end
 
   create_table "lyrics", force: :cascade do |t|
     t.string "phrase", null: false
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2023_07_10_132254) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "artists", "lyrics"
 end
