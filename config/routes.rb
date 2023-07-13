c19e52f83e81b02d3896ed41e01d334b7616b7ca
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-13 13:18:56 +0900

    Add UserProfile model #11

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root 'lyrics#index'
   resources :lyrics, only: [:new, :create, :edit, :update, :destroy, :show]
   resources :users, only: [:show]
