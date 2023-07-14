acca32bd47dde4ce10cd0ba8de70b44f100b7ce5
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-14 17:33:12 +0900

    Rename column type

--- app/models/feeling.rb
+++ app/models/feeling.rb
@@ -1,5 +1,4 @@
 class Feeling < ApplicationRecord
   has_many :lyrics_feelings, dependent: :destroy
   has_many :lyrics, through: :lyrics_feelings, source: :lyric
-  self.inheritance_column = :_type_disabled
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_14_074627) do
+ActiveRecord::Schema.define(version: 2023_07_14_083023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2023_07_14_074627) do
   end
 
   create_table "feelings", force: :cascade do |t|
-    t.string "type"
+    t.string "sort"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
