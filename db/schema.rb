127a855826254c9fd6cc8747713a176bd6682152
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-14 16:07:00 +0900

    Add null false to artist_name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_14_070403) do
+ActiveRecord::Schema.define(version: 2023_07_14_070618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "artists", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.bigint "lyric_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
