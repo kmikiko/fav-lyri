a38e050df4f8e0b0347c81de893ffe69a3262eac
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-14 15:47:35 +0900

    Set API key for Spotify

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -1,4 +1,7 @@
 class LyricsController < ApplicationController
+  require 'rspotify'
+  RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET_ID'])
+  
   before_action :authenticate_user!, only: [:new, :create]
   before_action :set_lyric, only: [:show, :edit, :update, :destroy]
   def index
