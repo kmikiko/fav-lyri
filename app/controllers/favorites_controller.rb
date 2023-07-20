a1bbcdad02ea314601b9cf3f5551a1fadc2425ca
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-20 16:24:31 +0900

    Modify favorites controller (redirect) #21

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,11 +2,11 @@ class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.create(lyric_id: params[:lyric_id])
     # render json: { message: 'お気に入りに登録しました' }, status: :created
-    # redirect_to lyrics_path, notice: "#{favorite.lyric.user.user_profile.name}さんの投稿をお気に入り登録しました"
+    redirect_to lyrics_path, notice: "#{favorite.lyric.user.user_profile.name}さんの投稿をお気に入り登録しました"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    # redirect_to lyrics_path, notice: "#{favorite.lyric.user.user_profile.name}さんの投稿をお気に入り解除しました"
+    redirect_to lyrics_path, notice: "#{favorite.lyric.user.user_profile.name}さんの投稿をお気に入り解除しました"
   end
 end
