6d171410531c04c0521b23d1ae57cc7f8608f4fb
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-21 14:15:59 +0900

    Add association between user and relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
   has_many :comments
   has_many :favorites, dependent: :destroy
   has_many :favorite_lyrics, through: :favorites, source: :lyric
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 end
