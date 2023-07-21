1ada0ad558b7080b88be63ae8f0e87eb96eaadd4
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-21 14:29:51 +0900

    Add routes for relationships #create #destroy

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   end
 
   resources :favorites, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
