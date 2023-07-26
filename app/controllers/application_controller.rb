4819458c8635fc7378138bb396438af29bec3b85
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-26 22:46:22 +0900

    Add notification #index view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_unchecked_notifications, if: :user_signed_in?
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [user_profile_attributes: [:id, :name, :icon, :image_cache, :introduction]])
     devise_parameter_sanitizer.permit(:account_update, keys: [user_profile_attributes: [:id, :name, :icon, :image_cache, :introduction]])
+  end
+
+  private
+
+  def set_unchecked_notifications
+    @unchecked_notifications = unchecked_notifications
+  end
+
+  def unchecked_notifications
+    current_user.passive_notifications.where(checked: false)
   end
 end
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,8 @@
 class NotificationsController < ApplicationController
   def index
-    @notifications = current_user.passive_notifications
-    @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
+    @notifications = current_user.passive_notifications.where(checked: false).to_a
+    @notifications.each do |notification|
+      notification.update(checked: true)
     end
   end
 end
