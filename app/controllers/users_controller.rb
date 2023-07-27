b269dd357914c40eee654f0ee83ae340c2e16a79
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-27 22:57:21 +0900

    Add #guest_admin_sign_in to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,14 @@ class UsersController < ApplicationController
 
   def guest_sign_in
     user = User.guest
-    
-
     sign_in user
     redirect_to root_path, notice: "ゲストユーザーとしてログインしました"
   end
 
+  def guest_admin_sign_in
+    user = User.guest_admin
+    sign_in user
+    redirect_to root_path, notice: 'ゲストユーザー（管理者）としてログインしました'
+  end
   
 end
