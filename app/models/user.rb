abf994a7cb953f38cabe0cb29f2d7593516e86d4
Kanda Mikiko <kmikiko1121@gmail.com>
2023-07-27 22:31:15 +0900

    Add guest_login link to global navi

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,4 +39,11 @@ class User < ApplicationRecord
       notification.save if notification.valid?
     end
   end
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲスト"
+    end
+  end
 end
