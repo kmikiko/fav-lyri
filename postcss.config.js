c58a246c24d204f72b50ae67942bce72c7987909
Kanda Mikiko <kmikiko1121@gmail.com>
2023-08-08 13:42:39 +0900

    Add introduction page #34

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   plugins: [
+    require('tailwindcss')({
+      theme: {
+        extend: {
+          fontFamily: {
+            sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+          },
+        },
+      },
+    }),
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
-        flexbox: 'no-2009'
+        flexbox: 'no-2009',
       },
-      stage: 3
-    })
-  ]
-}
+      stage: 3,
+    }),
+  ],
+};
