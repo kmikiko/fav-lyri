4f52a858173e4b8dfee95ab6a7d0f1f6fd1c25eb
Kanda Mikiko <kmikiko1121@gmail.com>
2023-08-13 15:33:52 +0900

    deploy43

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
                 plugins: {
                   autoprefixer: {},
                   cssnano: {},
-                  // 他のプラグイン
+                  postcss-merge-longhand: {},
                 },
               },
             },
