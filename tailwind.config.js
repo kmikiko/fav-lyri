e67b0323dd841cb438d42ff0b60fa3320609686c
Kanda Mikiko <kmikiko1121@gmail.com>
2023-08-13 15:15:51 +0900

    deploy41

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       './public/*.html',
       './app/helpers/**/*.rb',
       './app/javascript/**/*.js',
-      './app/views/**/*.{erb,haml,html,slim}'
+      './app/views/**/*.erb'
     ],
   },
   theme: {
