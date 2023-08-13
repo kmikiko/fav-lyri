3fd92e221a3db91ea28a0eddafa3305e34db8dbf
Kanda Mikiko <kmikiko1121@gmail.com>
2023-08-13 14:41:25 +0900

    deploy34

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,21 @@ const tailwindConfig = require('./tailwind.config.js');
 const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...fullConfig.theme.fontFamily.sans],
       },
     },
+  },
+  purge: {
+    enabled: true,
+    content: [
+      './public/*.html',
+      './app/helpers/**/*.rb',
+      './app/javascript/**/*.js',
+      './app/views/**/*.{erb,haml,html,slim}'
+    ],
   },
   plugins: [
     require('@tailwindcss/forms'),
