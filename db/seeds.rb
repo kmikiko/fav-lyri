5468594551ce69a9d49555575b9284040bd167b2
Kanda Mikiko <kmikiko1121@gmail.com>
2023-08-14 14:24:33 +0900

    Fix seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@
 user_names = ["John", "Jane", "Michael", "Emily", "David"]
 user_names.each do |name|
   user = User.create(
-    email: Faker::Internet.unique.email,
+    email: "#{name.downcase}@example.com",
     password: "password",
     confirmed_at: Time.now
   )
@@ -24,9 +24,26 @@ user_names.each do |name|
   )
 end
 
+song_titles = ["マリーゴールド", "うっせぇわ", "プロローグ", "カブトムシ", "怪獣の花唄"]
+
 artist_names = ["あいみょん", "Ado", "Uru", "aiko", "Vaundy"]
-artist_names.each do |name|
-  artist = Artist.create(name: name, lyric: Lyric.create(phrase: "Lyric phrase for #{name}", detail: "Lyric detail for #{name}", user: User.all.sample))
+artist_names.each_with_index do |name, index|
+  user = User.all.sample
+  lyric = Lyric.create(
+    phrase: "Lyric phrase for #{name}",
+    detail: "Lyric detail for #{name}",
+    user: user
+  )
+  
+  artist = Artist.create(name: name, lyric: lyric)
+
+  song_lyric = Lyric.create(
+    phrase: "Song lyric for #{song_titles[index]}",
+    detail: "Song lyric detail for #{song_titles[index]}",
+    user: user,
+    feelings: [Feeling.all.sample]
+  )
+  Song.create(title: song_titles[index], lyric: song_lyric)
 end
 
 comment_contents = ["めっちゃいい歌詞！", "この歌詞好きです。", "何度も聞くと意味がわかってきた！", "私もこれ好き！", "やばい！"]
@@ -35,12 +52,4 @@ Lyric.all.each do |lyric|
   
   Comment.create(content: comment_contents.sample, lyric: lyric, user: user)
   Favorite.create(user: user, lyric: lyric)
-end
-
-lyric_phrases = ["歌詞1", "歌詞2", "歌詞3", "歌詞4", "歌詞5"]
-Lyric.all.each do |lyric|
-  lyric.update(feelings: [Feeling.all.sample])
-end
-lyric_phrases.each do |phrase|
-  Lyric.create(phrase: phrase, detail: "Lyric detail for #{phrase}", user: User.all.sample, feelings: [Feeling.all.sample])
 end
