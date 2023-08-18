9a60402b7f195e29abb0b0cb7892389b7ac5ac91
Kanda Mikiko <kmikiko1121@gmail.com>
2023-08-18 17:36:04 +0900

    Apply Ajax to Favorites #54

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,18 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :authenticate_user!, only: %i[create destroy]
+  before_action :set_lyric, only: %i[create destroy]
   def create
-    favorite = current_user.favorites.create(lyric_id: params[:lyric_id])
-    lyric = Lyric.find(params[:lyric_id])
-    lyric.create_notification_favorite!(current_user)
-    redirect_to lyrics_path, notice: "#{favorite.lyric.user.user_profile.name}さんの投稿をお気に入り登録しました"
+    @favorite = current_user.favorites.create(lyric_id: params[:lyric_id])
+    @lyric.create_notification_favorite!(current_user)
   end
 
   def destroy
-    favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to lyrics_path, notice: "#{favorite.lyric.user.user_profile.name}さんの投稿をお気に入り解除しました"
+    @favorite = current_user.favorites.find_by(id: params[:id]).destroy
+  end
+
+  private
+
+  def set_lyric
+    @lyric = Lyric.find(params[:lyric_id])
   end
 end
